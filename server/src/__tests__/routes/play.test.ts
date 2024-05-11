316f8b8958d9f7f07dc30e1491370c26154aae9a
Jun Lee <junealee07@gmail.com>
2024-05-10 22:37:10 -0700

    fix play route tests

--- server/src/__tests__/routes/play.test.ts
+++ server/src/__tests__/routes/play.test.ts
@@ -2,12 +2,17 @@ import request from 'supertest';
 import express from 'express';
 import router from '../../routes/gameRoutes';
 import { difficultyLevels } from '../../configs/difficultySettings';
+import { beforeEach } from 'node:test';
+import { initialGameData } from '../../configs/initialGameData';
 
 const app = express();
 app.use(express.json());
 app.use('/', router);
 
 const testGamePlay = async (guess: string, difficultyLevel: string) => {
+  await request(app).post('/start').send({
+    difficultyLevel,
+  });
   const response = await request(app)
     .post('/play')
     .send({
@@ -32,10 +37,10 @@ describe('Game Routes', () => {
   it('plays the game successfully for Easy difficultyLevel', async () => {
     await testGamePlay('123', 'Easy');
   });
-  // it('plays the game successfully for Normal difficultyLevel', async () => {
-  //   await testGamePlay('1234', 'Normal');
-  // });
-  // it('plays the game successfully for Hard difficultyLevel', async () => {
-  //   await testGamePlay('12345', 'Hard');
-  // });
+  it('plays the game successfully for Normal difficultyLevel', async () => {
+    await testGamePlay('1234', 'Normal');
+  });
+  it('plays the game successfully for Hard difficultyLevel', async () => {
+    await testGamePlay('12345', 'Hard');
+  });
 });
